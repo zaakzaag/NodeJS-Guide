3c63c7dc599ea259c8a46f111284435bd9ca9bf1
Maciej <mocador23@o2.pl>
2021-06-21 11:59:15 +0200

    adding static css

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-
+const rootDir = require("./util/path");
 const app = express();
 
 const adminRoutes = require("./routes/admin");
@@ -9,12 +9,13 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views/404.html"));
+  res.status(404).sendFile(path.join(rootDir, "views/404.html"));
 });
 
 app.listen(3000);
