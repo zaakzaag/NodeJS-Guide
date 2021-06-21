2b5ae5ea12d6ed4cac53a87f73c54f5ed04e579b
Maciej <mocador23@o2.pl>
2021-06-21 11:18:26 +0200

    added 404

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(adminRoutes);
-
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1> 404 PAGE NOT FOUND</h1>");
+});
 
 app.listen(3000);
 // const server = http.createServer(app);
